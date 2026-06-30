211943953e83b484c6d735a78e1a5d9bbb78c173
JannisNe <necker@physik.hu-berlin.de>
2026-06-30 16:11:51 +0200

    test listening function

--- tests/test_listen.py
+++ tests/test_listen.py
@@ -4,13 +4,14 @@ from hop import Stream
 from hop.auth import Auth
 from hop.io import StartPosition
 
+from tdemocracy.listen import listen_to_nuclear_stream
 from tdemocracy.model import LSSTReport
 from tdemocracy.settings import Settings
 
 LOGGER = logging.getLogger(__name__)
 
 
-def test_listen_to_nuclear_stream(tmp_path):
+def test_manual_listen_to_nuclear_stream():
     logging.basicConfig(level=logging.DEBUG)
     _settings = Settings()
     auth = Auth(_settings.username, _settings.password)
@@ -28,3 +29,8 @@ def test_listen_to_nuclear_stream(tmp_path):
 
     for m in messages[-2:]:
         assert LSSTReport.model_validate(m.content) is not None
+
+
+def test_listen_to_nuclear_stream():
+    reports = list(listen_to_nuclear_stream(until_eos=True))
+    assert len(reports) > 0
