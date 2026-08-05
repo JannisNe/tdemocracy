c64ea02b66eac1e873da1cda844b9c9b8fded91f
JannisNe <necker@physik.hu-berlin.de>
2026-08-05 18:41:42 +0200

    parse older model versions

--- tdemocracy/listen.py
+++ tdemocracy/listen.py
@@ -2,11 +2,13 @@ import logging
 from collections.abc import Generator
 from typing import Any
 
+import numpy as np
 from hop import Stream
 from hop.auth import Auth
 from hop.io import StartPosition
+from packaging.version import Version
 
-from tdemocracy import __version__ as tdemocracy_version
+from tdemocracy import __version__ as tdemocracy_version_str
 from tdemocracy.model import NuclearTransientReport
 from tdemocracy.settings import Settings
 
@@ -17,7 +19,6 @@ def listen_to_nuclear_stream(
     start_at: Any = StartPosition.EARLIEST,
     until_eos: bool = False,
     settings: Settings | None = None,
-    skip_other_versions: bool = False,
 ) -> Generator[NuclearTransientReport]:
     """
     Listen to Nuclear stream
@@ -28,22 +29,38 @@ def listen_to_nuclear_stream(
     :type until_eos: bool
     :param settings: Settings to use, if not passed reads the corresponding from the environment or a `.env` file. See :class:`tdemocracy.settings.Settings` for details.
     :type settings: Settings | None
-    :param skip_other_versions: Skip alerts with wrong versions (default)
-    :type skip_other_versions: bool
     """
     _settings = settings or Settings()
     auth = Auth(_settings.username, _settings.password.get_secret_value())
     stream = Stream(auth=auth, start_at=start_at, until_eos=until_eos)
+    tdemocracy_version = Version(tdemocracy_version_str)
 
     with stream.open(f"kafka://kafka.scimma.org/{_settings.topic}", mode="r", group_id=_settings.group_id) as s:
         LOGGER.info(f"Listening to {_settings.topic}...")
         for message in s:
             LOGGER.debug("Received message")
-            if ((mv := message.content.get("model_version")) is None) or (mv != tdemocracy_version):
-                if skip_other_versions:
-                    LOGGER.debug(f"Skipping version {mv}")
-                    continue
-                else:
-                    msg = f"Received message with wrong model_version: {mv}, expected: {tdemocracy_version}"
-                    raise ValueError(msg)
-            yield NuclearTransientReport.model_validate(message.content)
+            content = message.content
+            mv_str = content["version"]
+
+            if mv_str in {"0.0.6", "0.0.5"}:
+                yield NuclearTransientReport.model_validate(content)
+
+            elif mv_str in {"0.0.4", "0.0.3", "0.0.2", "0.0.1"}:
+                # backwards compatibility for older model version
+                content["host"]["ra"] = np.nan
+                content["host"]["dec"] = np.nan
+                content["host"]["sources"] = content["host"]["source"]
+                content["host"]["primary_source"] = content["host"]["source"][0]
+                yield NuclearTransientReport.model_validate(content)
+
+            elif Version(mv_str) > tdemocracy_version:
+                # raise error if latest version is not installed
+                msg = (
+                    f"Received model version v{mv_str}, installed version v{tdemocracy_version}. "
+                    f"Please update tdemocracy!"
+                )
+                raise ValueError(msg)
+
+            else:
+                msg = f"Can not parse model_version: {mv_str}!"
+                raise ValueError(msg)
